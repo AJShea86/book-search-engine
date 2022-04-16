8163bfd8cc34a018401561838fd7929067f05a8f
Andrew S <andrewsheamls@gmail.com>
2022-04-16 12:16:28 -0600

    back end working

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,68 @@
+const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { input }, { user }) => {
+      if (!user) {
+        throw new AuthenticationError("You need to be logged in.");
+      } else {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: user._id },
+          { $addToSet: { savedBooks: input } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      }
+    },
+    removeBook: async (parent, {bookId}, {user}) =>{
+      if (!user) {
+        throw new AuthenticationError('You need to be logged in.')
+      } else {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+    
+
+        )
+        return updatedUser;
+      }
+
+    }
+  },
+};
+
+module.exports = resolvers;
